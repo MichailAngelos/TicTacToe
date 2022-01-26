50f8a8997bb55ed5f999890987cebf9b2c9cb3db
Michail Angelos Georgiou <michailangelos.georgiou@gmail.com>
2022-01-26 10:06:07 +0200

    remove rendundant code and comment improvement

--- class.tictactoe.php
+++ class.tictactoe.php
@@ -2,19 +2,18 @@
 
 class tictactoe extends game
 {
-    var $player = "X";            //seira paikti "X"
-    var $board = array();        //o pinakas
-    var $totalMoves = 0;        //poses kinisis exoun ginei
+    var $player = "X";      
+    var $board = array();
+    var $totalMoves = 0;
 
-    /** Default constructor pou dixnei kai tin enarksi leitourgias tis gonikis klasis**/
+    /** Default constructor **/
     function tictactoe()
     {
-        /** KLironomi tis methodous kai xaraktiristika tis klasis game kai kalei tin methodo start **/
         game::start();
         $this->newBoard();
     }
 
-    /** Ekinisi neou paixnidiou  **/
+    /** Start of the game  **/
     function newGame()
     {
 
@@ -26,25 +25,17 @@ class tictactoe extends game
         $this->newBoard();
     }
 
-    /** Dimiourgeia adeiou pinaka**/
-
+    /** Creaton of board **/
     function newBoard()
     {
+    $this->board = array();
 
-
-        $this->board = array();
-
-        //dimiorgei to board
-        for ($x = 0; $x <= 2; $x++) {
+         for ($x = 0; $x <= 2; $x++) {
             for ($y = 0; $y <= 2; $y++) {
                 $this->board[$x][$y] = null;
             }
         }
     }
-
-
-    /** Trexei to block tou kwdika mexri na vgei isopalia i kapios na kerdisei **/
-
 
     function playGame($gamedata)
     {
@@ -61,15 +52,12 @@ class tictactoe extends game
             $this->theComputer();
         }
 
-        //display the game
         $this->displayGame();
     }
-
-    /** Emfanisei tou interface mas**/
+	
     function displayGame()
     {
 
-        //Oso to paixnidi den telinei isOver()
         if (!$this->isOver()) {
             echo "<div id=\"board\">";
 
@@ -77,23 +65,19 @@ class tictactoe extends game
                 for ($y = 0; $y < 3; $y++) {
                     echo "<div class=\"board_cell\">";
 
-                    //elexi an einai gemati i thesi
+                    //Check position for value
                     if ($this->board[$x][$y])
                         echo "<img src=\"/{$this->board[$x][$y]}.jpg\" alt=\"{$this->board[$x][$y]}\" title=\"{$this->board[$x][$y]}\" />";
                     else {
-                        //epilogis X i 0
                         echo "<select name=\"{$x}_{$y}\">
 								<option value=\"\"></option>
 								<option value=\"{$this->player}\">{$this->player}</option>
 							</select>";
                     }
-
                     echo "</div>";
                 }
-
                 echo "<div class=\"break\"></div>";
             }
-
             echo "
 				<p align=\"center\">
 					<input type=\"submit\" name=\"move\" value=\"Take Turn\" />
@@ -104,8 +88,7 @@ class tictactoe extends game
 					</p>
 			</div>";
         } else {
-
-            //kapios kerdise i isopalia
+		
             if ($this->isOver() != "Tie")
                 echo successMsg("Congratulations player " . $this->isOver() . ", you've won the game!");
             else if ($this->isOver() == "Tie")
@@ -115,19 +98,13 @@ class tictactoe extends game
 
             echo "<p align=\"center\"><input type=\"submit\" name=\"newgame\" value=\"New Game\" /></p>";
         }
-
-
     }
-
-    /** Kinisis mexri tin liksi tou painxidiou **/
 
     function move($gamedata)
     {
-
         if ($this->isOver())
             return;
 
-        //na min exoume dipla sto pinaka(unique)
         $gamedata = array_unique($gamedata);
 
         foreach ($gamedata as $key => $value) {
@@ -149,11 +126,10 @@ class tictactoe extends game
         if ($this->isOver())
             return;
     }
-    /** Random gemisma apo Computer **/
-
+   
+     /** Math Random for single player **/
     function theComputer()
     {
-
         if ($this->player == "X")
             $this->player = "O";
         else
@@ -173,65 +149,41 @@ class tictactoe extends game
 
     }
 
-    /** Elexos nikiti an yparxei allios emfanisei isopalias**/
     function isOver()
     {
-
-
-//        for($x = 0; $x < count($this->board);$x++){
-//            if($count == 3)
-//                return true;
-//
-//            $column = $this->board[$x];
-//            $count = 1;
-//            for($y = 0; $y < count($column);$y++){
-//                if($count == 1){
-//                    $value = $column[$y];
-//                    continue 2;
-//                }
-//
-//                if($value != $column[$y])
-//                    break 2;
-//
-//                count++;
-//            }
-//        }
-//
-
-        // 1
+        // 1 row
         if ($this->board[0][0] && $this->board[0][0] == $this->board[0][1] && $this->board[0][1] == $this->board[0][2])
             return $this->board[0][0];
 
-        //2
+        //2 row
         if ($this->board[1][0] && $this->board[1][0] == $this->board[1][1] && $this->board[1][1] == $this->board[1][2])
             return $this->board[1][0];
 
-        //3
+        //3 row
         if ($this->board[2][0] && $this->board[2][0] == $this->board[2][1] && $this->board[2][1] == $this->board[2][2])
             return $this->board[2][0];
 
-        //prwti stili
+        //first column
         if ($this->board[0][0] && $this->board[0][0] == $this->board[1][0] && $this->board[1][0] == $this->board[2][0])
             return $this->board[0][0];
 
-        //deuteri stili
+        //second column
         if ($this->board[0][1] && $this->board[0][1] == $this->board[1][1] && $this->board[1][1] == $this->board[2][1])
             return $this->board[0][1];
 
-        //triti stili
+        //third column
         if ($this->board[0][2] && $this->board[0][2] == $this->board[1][2] && $this->board[1][2] == $this->board[2][2])
             return $this->board[0][2];
-
-        //prwti diagonios
+	
+	//first diagonal line
         if ($this->board[0][0] && $this->board[0][0] == $this->board[1][1] && $this->board[1][1] == $this->board[2][2])
             return $this->board[0][0];
 
-        //deuteri diagonios
+	//second diagonal line    
         if ($this->board[0][2] && $this->board[0][2] == $this->board[1][1] && $this->board[1][1] == $this->board[2][0])
             return $this->board[0][2];
 
         if ($this->totalMoves >= 9)
             return "Tie";
     }
-
 }
